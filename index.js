8eb04550929c379637f9414ea501ca05a05506e9
Graham Krizek <graham.krizek@emailonacid.com>
2016-12-07 14:25:28 -0700

    added routes

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
 var express = require('express');
 var request = require('request');
 
+//  Stored in Environment Variables
+//var clientId = '';
+//var clientSecret = '';
 
+var app = express();
+
+const PORT=8080;
+
+app.listen(PORT, function () {
+    console.log("Example app listening on port " + PORT);
+});
+
+app.get('/', function(req, res) {
+    res.send('Slack App is Working');
+});
+
+app.get('/oauth', function(req, res) {
+    if (!req.query.code) {
+        res.status(500);
+        res.send({"Error": "Looks like we're not getting code."});
+        console.log("Looks like we're not getting code.");
+    } else {
+        request({
+            url: 'https://slack.com/api/oauth.access',
+            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret},
+            method: 'GET', 
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.json(body);
+            }
+        })
+    }
+
+    app.post('/command', function(req, res) {
+        res.send(req);
+    });
+});
