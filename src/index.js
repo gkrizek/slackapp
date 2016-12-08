1eddcc0f6a85b9d829d3961929ab6c90ad59a2d6
Graham Krizek <graham@grahamkrizek.com>
2016-12-07 22:25:16 -0700

    able to run commands from slack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var request = require('request');
+var bodyParser = require('body-parser');
 var exec = require('child_process').exec;
 
 //  Stored in Environment Variables
@@ -14,6 +15,8 @@ app.listen(PORT, function () {
     console.log("Example app listening on port " + PORT);
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function(req, res) {
     res.send('Slack App is Working');
 });
@@ -24,14 +27,11 @@ app.get('/oauth', function(req, res) {
         res.send({"Error": "Looks like we're not getting code."});
         console.log("Looks like we're not getting code.");
     } else {
-        //console.log(req);
         request({
             url: 'https://slack.com/api/oauth.access',
             qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret},
             method: 'GET', 
         }, function (error, response, body) {
-            //console.log(response);
-            //console.log(body);
             if (error) {
                 console.log(error);
             } else {
@@ -43,19 +43,19 @@ app.get('/oauth', function(req, res) {
 
 });
 
-    app.post('/command', function(req, result) {
-        console.log(req.route);
-        var command = "pwd";
+    app.post('/command', function(req, res) {
+        console.log(req.body.text);
+        var command = req.body.text;
         exec(command, function(error, stdout, stderr) {
             if(stderr){
-                result.send(stderr);
+                res.send(stderr);
             }else if(error){
                 console.log(error);
-                result.send('There was a problem');
+                res.send('There was a problem');
             }else{
-                var test = {"text": "It\'s 80 degrees right now.","attachments":[{"text":"```Partly cloudy today and tomorrow```"}]};
+                var test = {"text": "Response from Brix:","attachments":[{"text":"```"+stdout+"```", "mrkdwn_in": ["text"]}]};
                 //result.send(stdout);
-                result.send(test);
+                res.send(test);
             }
         });
     });
