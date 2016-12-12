92bf3a3b11a88986987229edb5760d9868ae3cc7
Graham Krizek <graham@grahamkrizek.com>
2016-12-11 22:26:14 -0700

    added file notification mechanism

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ var bodyParser = require('body-parser');
 var exec = require('child_process').exec;
 fs = require('fs');
 var log = console.log;
-//  Stored in Environment Variables
-//var clientId = '';
-//var clientSecret = '';
+
+client_id = process.env.CLIENT_ID;
+client_secret = process.env.CLIENT_SECRET;
+apiKey = process.env.API_KEY;
 
 var app = express();
 
@@ -47,6 +48,7 @@ app.get('/oauth', function(req, res) {
     app.post('/krate', function(req, res) {
         var text = req.body.text;
         var response_url = req.body.response_url;
+        var channel_id = req.body.channel_id;
         var team_id = req.body.team_id;
         var command = text.split(' ')[0];
         res.send({"text": "Request received..."});
@@ -54,13 +56,15 @@ app.get('/oauth', function(req, res) {
             case "ssh":
                 execute(text, team_id, response_url);
             case "init":
-                init(text);
+                init(text, channel_id, team_id, response_url);
+            case "initsave":
+                initsave(text, channel_id, team_id, response_url);
         }
     });
 
 
     function execute(text, team_id, response_url){
-        var command = text.split(/ (.+)/)[1]
+        var command = text.split(/ (.+)/)[1];
         exec(command, function(error, stdout, stderr) {
             if(stderr){
                 var test = {"text": "Response from Krate:","username": "Krate","attachments":[{"text":"```"+stderr+"```","color": "#ff0000","mrkdwn_in": ["text"]}]};
@@ -84,54 +88,69 @@ app.get('/oauth', function(req, res) {
                     } else {
                         log(body);
                     }
-                })
-
-
+                });
             }
         });
     }
 
-    function init(text){
+    function init(text, channel_id, team_id, response_url){
+        var filename = text.split(/ (.+)/)[1];
+        var file = "project_name: "+filename+"\ngit_url: GIT_URL";
+        var token = apiKey;
+        request({
+            url: 'https://slack.com/api/files.upload',
+            qs: {token: token, filename: filename, channels: channel_id, content: file},
+            method: 'POST',
+        }, function (error, response, body) {
+            //log(JSON.parse(body));
+        });
+    }
 
+    function initsave(text, channel_id, team_id, response_url){
+        var token = apiKey;
+        request({
+            url: 'https://slack.com/api/files.list',
+            qs: {token: token, channel: channel_id},
+            method: 'POST',
+        }, function (error, response, body) {
+            var result = JSON.parse(body);
+            var url = result.files[0].url_private;
+
+            request({
+                url: url,
+                method: 'GET',
+                headers: {'Authorization': "Bearer "+token}
+            }, function (error, response, body) {
+                fs.writeFile('../'+result.files[0].name, body, function(err){
+                    if(err){
+                        log(err);
+                    }
+                    notifySuccess(response_url);
+                });
+            });
+
+        });
+    }
+
+
+    function notifySuccess(response_url){
+        var body = {"text": "Successfully saved file!","username": "Krate"};
+        request({
+            url: response_url,
+            json: true,
+            headers: {'content-type': 'application/json'},
+            body: body,
+            method: 'POST'
+        }, function (error, response, body) {
+            if (error) {
+               console.log(error);
+            } else {
+                log(body);
+            }
+        });
     }
 
 
 
-app.post('/test', function(req, res) {
-    var token = "";
-    var channel = req.body.channel_id;
-    //console.log(channel);
-    request({
-        url: 'https://slack.com/api/files.info',
-        //qs: {token: token, channel: channel},
-        qs: {token: token, file: 'F3DQM35QF'},
-        method: 'GET',
-    }, function (error, response, body) {
-        //console.log(error);
-        //console.log(response);
-        var json = JSON.parse(body);
-        console.log(json);
-        //var url = json.file.url_private_download;
-        var url = 'https://files.slack.com/files-pub/T3C89DL30-F3DQM35QF/download/testing.js';
-        var r = request(url);
-        r.on('response',  function (res) {res.pipe(fs.createWriteStream('/Users/AppleUser/Desktop/file.js'));});
-    //})
-});
-});
-
-app.post('/upload', function(req, res) {
-    var token = "";
-    var channel = req.body.channel_id;
-    var command = req.body.text;
-    var file = fs.readFileSync(command).toString();
-    request({
-        url: 'https://slack.com/api/files.upload',
-        qs: {token: token, filename: command, channels: channel, content: file},
-        method: 'POST',
-    }, function (error, response, body) {
-        log(JSON.parse(body));
-    });
-
-});
 
 
